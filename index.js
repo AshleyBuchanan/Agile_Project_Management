f7726d859ff5a884d104fc61456b92d59e02579e
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-18 16:46:17 -0700

    added necessary node libraries and a nifty icon. added public and views folders with js and css files.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('main'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
