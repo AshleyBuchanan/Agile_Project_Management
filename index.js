fa308d5adca1809d022cb71b8feedafb61827b6e
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-18 20:15:52 -0700

    finished navbar framework

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('main'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
