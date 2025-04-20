53ffbacd33ec7022fc4f6c997920a0f9b5cca0cb
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-19 19:55:00 -0700

    added dashboard panel and board. started to implement ejs related data

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+const user = {
+    name: 'Jasdeep',
+    team: 'FM BE and Web',
+    projects:
+        [
+            'FM Member Feedback - FY25 Q3 5 issues',
+            'FM Month in Review - FY25 Q3 5 issues'
+        ]
+};
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('main'));
+app.get('/', (req, res) => {
+    const panelNames = ['START', 'BLOCKED', 'IN PROGRESS', 'CODE REVIEW', 'TESTING', 'QE VALIDATED', 'DONE'];
+    res.render('main', { panelNames, user });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
