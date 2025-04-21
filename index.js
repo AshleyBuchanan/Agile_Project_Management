256d932d1cb13f4bf854b688ffcc502726f93d5b
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-04-21 14:14:20 -0700

    cards (tickets) are displayed correcty in their respective project panels. add some background color.

--- index.js
+++ index.js
@@ -4,27 +4,44 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-const users = [{
-    name: 'Jasdeep',
-    team: 'FM BE and Web',
-    projects:
-        [
-            'FM Member Feedback - FY25 Q3',
-            'FM Month in Review - FY25 Q3',
-            'FM Some other Task - FY25 Q3',
-        ],
-    tickets:
-        [
-            'MINT-4748',
-            'MINT-4749',
-            'MINT-4742',
-            'MINT-4743',
-            'MINT-4723',
-            'MINT-4606',
-            'MINT-4605',
-            'MINT-4604',
-        ]
-}];
+const users = [
+    {
+        name: 'Jasdeep',
+        team: 'FM BE and Web',
+        projects:
+            [
+                'FM Member Feedback - FY25 Q3',
+                'FM Month in Review - FY25 Q3',
+                'FM Some other Task - FY25 Q3',
+            ],
+        tickets:
+            [
+                'MINT-4748',
+                'MINT-4749',
+                'MINT-4742',
+                'MINT-4743',
+                'MINT-4723',
+                'MINT-4606',
+                'MINT-4605',
+                'MINT-4604',
+            ]
+    },
+    {
+        name: 'Vinita',
+        team: 'FM BE and Web',
+        projects:
+            [
+                'FM Member Feedback - FY25 Q3',
+                'FM Month in Review - FY25 Q3',
+                'FM Some other Task - FY25 Q3',
+            ],
+        tickets:
+            [
+                'MADEUP-9999',
+                'MADEUP-9998',
+            ]
+    }
+];
 
 const tickets = [
     {
@@ -32,20 +49,25 @@ const tickets = [
         title: 'MINT-4748',
         description: 'Add unit test for month in review surface',
         priority: 'major',
+        hierarchy: 'story',
         step: 'START',
+        epic: true,
     },
     {
         project: 'FM Month in Review - FY25 Q3',
         title: 'MINT-4749',
         description: 'Tracking for Month In Review feature',
         priority: 'major',
+        hierarchy: 'story',
         step: 'START',
+        epic: true,
     },
     {
         project: 'FM Month in Review - FY25 Q3',
         title: 'MINT-4742',
         description: 'Create Fabric component for Unconnected User',
         priority: 'major',
+        hierarchy: 'bug',
         step: 'IN PROGRESS',
         epic: true,
     },
@@ -54,6 +76,7 @@ const tickets = [
         title: 'MINT-4743',
         description: 'Create and render templates for Unconnected User',
         priority: 'major',
+        hierarchy: 'bug',
         step: 'IN PROGRESS',
         epic: true,
     },
@@ -62,6 +85,7 @@ const tickets = [
         title: 'MINT-4723',
         description: 'Create darwin experiment for the feature',
         priority: 'major',
+        hierarchy: 'story',
         step: 'DONE',
         epic: true,
     },
@@ -70,6 +94,7 @@ const tickets = [
         title: 'MINT-4606',
         description: 'Fetch data needed for takeover content and eligibility',
         priority: 'major',
+        hierarchy: 'story',
         step: 'DONE',
         epic: false,
     },
@@ -78,6 +103,7 @@ const tickets = [
         title: 'MINT-4605',
         description: 'Build bottom takeover Fabric component',
         priority: 'major',
+        hierarchy: 'story',
         step: 'DONE',
         epic: false,
     },
@@ -86,6 +112,16 @@ const tickets = [
         title: 'MINT-4604',
         description: 'Implement additional events for tracking Member Feedback',
         priority: 'major',
+        hierarchy: 'story',
+        step: 'DONE',
+        epic: false,
+    },
+    {
+        project: 'FM Some other Task - FY25 Q3',
+        title: 'MADEUP-9999',
+        description: 'Fix login issue',
+        priority: 'blocker',
+        hierarchy: 'bug',
         step: 'DONE',
         epic: false,
     },
--- public/app.js
+++ public/app.js
@@ -1,17 +1,77 @@
 const panels = document.querySelectorAll('.panel');
+let userData;
+const userNames = { names: ['Jasdeep', 'Vinita'] };
 
-const xhr = new XMLHttpRequest();
-xhr.open('POST', '/tickets', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
+function makeCardElement(c) {
+    const cardElement = document.createElement('div');
+    cardElement.className = 'card';
 
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        const userData = JSON.parse(xhr.responseText);
-        console.log('User Data:', userData);
-    } else {
-        console.error('Something went wrong:', xhr.status);
-    }
-};
+    const title = document.createElement('div');
+    title.className = 'card-title';
+    title.innerText = c.title;
 
-const userNames = { names: ['Jasdeep'] };
-xhr.send(JSON.stringify(userNames));
+    const description = document.createElement('div');
+    description.className = 'card-description';
+    description.innerText = c.description;
+
+    const project = document.createElement('div');
+    project.className = 'card-project major';
+    project.innerText = c.project;
+
+    const tagsLine = document.createElement('div');
+    tagsLine.className = 'card-tagsLine';
+
+    const hierarchy = document.createElement('img');
+    hierarchy.className = 'card-icon';
+    hierarchy.src = `/${c.hierarchy}_icon.png`;
+    tagsLine.append(hierarchy);
+
+    const priority = document.createElement('img');
+    priority.className = 'card-icon';
+    priority.src = `/${c.priority}_icon.png`;
+    tagsLine.append(priority);
+
+    const userText = document.createElement('div');
+    userText.className = 'card-user';
+    userText.innerText = 'Jasdeep';
+    tagsLine.append(userText);
+
+    cardElement.append(title);
+    cardElement.append(description);
+    cardElement.append(project);
+    cardElement.append(tagsLine);
+    return cardElement;
+}
+
+function placeCards(cards) {
+    cards.forEach(card => {
+        for (let panel of panels) {
+            if (card.project === panel.dataset.project && card.step === panel.dataset.step) {
+                const cardElement = makeCardElement(card);
+                panel.append(cardElement);
+            }
+        }
+    });
+}
+
+function inquire(names) {
+    let userData;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/tickets', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            userData = JSON.parse(xhr.responseText);
+            placeCards(userData);
+        } else {
+            console.error('Something went wrong:', xhr.status);
+        }
+    };
+
+    xhr.send(JSON.stringify(names));
+
+    return userData;
+}
+
+inquire(userNames);
